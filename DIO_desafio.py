bc2175d881035e5c3e55d68f81328e18f74099fb
Matheus Ribeiro <personalmatheus.rib@gmail.com>
2025-07-03 17:03:11 -0300

    Update DIO_desafio.py
    Foram adicionadas as funções de criação de conta, listar conta e criação de novos usuários conforme proposto no desfio do bootcamp.

--- DIO_desafio.py
+++ DIO_desafio.py
@@ -7,6 +7,9 @@ def menu():
     1. DEPÓSITO
     2. SAQUE
     3. EXTRATO
+    4. CRIAR USUÁRIO
+    5. CRIAR CONTA
+    6. LISTAR CONTA 
     0. SAIR 
     """)
     escolha = int(input("Selecione a opção desejada: "))
@@ -54,13 +57,61 @@ def exibir_extrato(saldo, /, *, extrato):
     print(f" \nSaldo:\t\tR$ {saldo:.2f}")
     print("=============================")
 
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente número): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        return
+
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
+
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\nUsuário não encontrado, fluxo de criação de conta encerrado!")
+
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(linha)
+
 
 def main():
     limite_saques = 3
+    agencia = "0001"
+    
     saldo = 0
     limite = 500
     extrato = ""
     numero_saques = 0
+    usuarios = []
+    contas = []
+    
 
     while True:
         opcao = menu()
@@ -83,11 +134,24 @@ def main():
         elif opcao == 3:
             exibir_extrato(saldo, extrato=extrato)
 
+        elif opcao == 4:
+            criar_usuario(usuarios)
+
+        elif opcao == 5:
+            numero_conta = len(contas) + 1
+            conta = criar_conta(agencia, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
+                
+        elif opcao == 6:
+            listar_contas(contas)
+
         elif opcao == 0:
             print("Encerrando o programa...")
             break
         else:
             print("OPERAÇÃO INVÁLIDA! TENTE NOVAMENTE.")
-
+main()
 
 main()
